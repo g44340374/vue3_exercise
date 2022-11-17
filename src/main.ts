d145319dcd6bb54d34f23f3de46f148a0b1e0679
guoyaowen <guoyaowen@shiqiao.com>
2022-11-17 09:06:33 +0800

    feat: 全局配置icon

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,20 @@ import 'normalize.css'; // 浏览器默认样式
 
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
 import piniaStore from './store';
 
 import App from './App.vue';
 import router from '@/router';
 
-createApp(App)
+const app = createApp(App);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
+app
   .use(piniaStore)
   .use(ElementPlus, { size: 'small', zIndex: 3000 })
   .use(router)
